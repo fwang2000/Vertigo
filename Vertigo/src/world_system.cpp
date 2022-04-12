244f3efe40b48d52fa9ce9fa4a1ee35e6e558cba
alex587 <alex587@students.cs.ubc.ca>
2022-04-11 21:31:12 -0700

    m4 submission

--- Vertigo/src/world_system.cpp
+++ Vertigo/src/world_system.cpp
@@ -14,7 +14,7 @@
 
 // Create the world
 WorldSystem::WorldSystem()
-	: level(18) {
+	: level(0) {
 	// Seeding rng with random device
 	// rng = std::default_random_engine(std::random_device()());
 }
